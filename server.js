2522d8f74dbd4f0f1794c175bf1f9827b74af997
Alice Chang <sinsofangels@gmail.com>
2020-12-02 13:47:06 -0500

    Add env variable motd

--- server.js
+++ server.js
@@ -7,7 +7,15 @@ const server = express();
 server.use(express.json());
 server.use("/posts", postRouter);
 server.use("/users", userRouter);
-
+server.get('/', async (req, res) => {
+  try {
+    const messageOfTheDay = process.env.MOTD || 'Hello World!';
+    res.status(200).json({ motd: messageOfTheDay});
+  } catch (error) {
+    console.error('\nERROR', error);
+    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
+  }
+});
 //custom middleware
 
 function logger(req, res, next) {
